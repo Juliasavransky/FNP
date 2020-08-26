a1e54475c74513af5d8a3939233b5db28fc477ad
Julia savransky <savranskyj@gmail.com>
2020-08-26 14:28:05 +0300

    first stap in smart com

--- src/pages/Smart agent/SmartAgent.js
+++ src/pages/Smart agent/SmartAgent.js
@@ -1,182 +1,157 @@
-// import React, { Component } from 'react';
-// import { Form, FormControl, Button, Col, Dropdown } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import AdCard from '../../components/AdCard/AdCard'
-// import { dataLivingAreas, dataConditions, dataSubCategorys, dataCategoriess } from '../../data/ddData'
+import React, { Component } from 'react';
+import { Form, FormControl, Button, Col, Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdCard from '../../components/AdCard/AdCard'
+import { dataLivingAreas, dataConditions, dataSubCategorys, dataCategoriess } from '../../data/ddData'
+
+class smartAgent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+
+        this.state = {
+            CategoryId: null,
+            SubCategoryId: null,
+            condition: null,
+            LivingArea: null,
+
+        };
+        console.log(this.state);
+
+        this.categoryChange = this.categoryChange.bind(this);
+        this.subCategoryChange = this.subCategoryChange.bind(this);
+        this.changeItemConditions = this.changeItemConditions.bind(this);
+        this.changeItemLivingArea = this.changeItemLivingArea.bind(this);
+        this.search = this.search.bind(this);
+
+    }
+
+    categoryChange = (item) => {
+
+        const { ads } = this.props;
+        this.setState({
+            CategoryId: item.CategoryId,
+
+        })
+
+    }
 
 
-// const adscategorys = { category: 'Select Category ...' };
-// const adssubCategory = { subCategory: 'Select Category ...' };
-// const adsCondition = { Condition: 'Select Category ...' };
-// const allUsersLivingArea = { LivingArea: 'Select Category ...' };
-// // console.log(dataConditions);
+    subCategoryChange = (item) => {
+        const { ads } = this.props;
+
+        this.setState({
+            SubCategoryId: item.SubCategoryId,
+        });
+    }
 
 
-// class smartAgent extends Component {
+    changeItemConditions = (item) => {
+        const { ads } = this.props;
 
-//     constructor(props) {
-//         super(props);
+        this.setState({
+            condition: item.condition
+        })
 
-//         console.log(props);
+    }
 
-//         this.state = {
-//             category: null,
-//             subCategory: null,
-//             Condition: null,
-//             LivingArea: null,
-//             subCategorys: dataSubCategorys,
-//             categorys: dataCategoriess,
-//             Conditions: dataConditions,
-//             LivingAreas: dataLivingAreas
+    changeItemLivingArea = (item) => {
+        const { allUsers } = this.props;
 
-//         };
-//         console.log(this.state);
+        this.setState({
+            LivingArea: item.LivingArea
+        })
 
-//         this.categoryChange = this.categoryChange.bind(this);
-//         this.subCategoryChange = this.subCategoryChange.bind(this);
-//         this.changeItemConditions = this.changeItemConditions.bind(this);
-//         this.changeItemLivingArea = this.changeItemLivingArea.bind(this);
-//         this.search = this.search.bind(this);
+    }
 
-//     }
+    search = (event) => {
+        this.setState({
+            search: event.target.value
 
-//     categoryChange = (event) => {
-
-//         const category = event.target.value;
-//         const subCategorys = ads.filter(ad => SubCategoryId === category);
-//         const subCategorys = dataSubCategorys.filter(SubCategory => SubCategory.Categoryid === Category.CategoryId);
+        })
 
 
-//         this.setState({
-//             categorys: category,
-//             subCategorys: subCategorys,
-//             category: category,
+    }
+
+    render() {
+        const { ads, search, activeUser, allUsers } = this.props;
+        const { category, subCategory, Conditions, LivingAreas } = this.state;
+
+        const categoryOption = dataCategoriess.map(itencategorys => <Dropdown.Item onClick={() => this.categoryChange(itencategorys)}> {itencategorys.categoryName} </Dropdown.Item>)
+        const filterdcategorys = ads.filter(ad => ad.categoryName === this.state.condition);
+
+        const fileredSubCategorys = dataSubCategorys.filter(dataSubCategory => dataSubCategory.CategoryId === this.state.CategoryId)
 
 
-//         })
-//         console.log(event);
+        const subCategoryOption = fileredSubCategorys.map(itenSubCategorys => <Dropdown.Item onClick={() => this.subCategoryChange(itenSubCategorys)} > {itenSubCategorys.SubCategoryName} </Dropdown.Item>)
+        const filterdSubCategorys = ads.filter(ad => ad.subCategoryName === this.state.condition);
 
-//     }
+        const itemConditionOption = dataConditions.map(itemCondition => <Dropdown.Item onClick={() => this.changeItemConditions(itemCondition)}> {itemCondition.ConditionName} </Dropdown.Item>)
+        const filterdConditions = ads.filter(ad => ad.Condition === this.state.condition);
 
-
-//     subCategoryChange = (event) => {
-//         const subCategory = event.target.value;
-//         const subCategorys = ads.filter(ad => SubCategoryId === category);
-
-
-//         this.setState({
-//             categorys: category,
-//             subCategorys: subCategorys,
-//             subCategory: event.target.value,
-
-//         });
-//         console.log(event);
-//     }
+        const dataLivingAreasOption = dataLivingAreas.map(LivingAreasOption => <Dropdown.Item onClick={() => this.changeItemLivingArea(LivingAreasOption)}> {LivingAreasOption.LivingAreaName} </Dropdown.Item>)
+        const filterdLivingArea = allUsers.filter(allUser => allUser.LivingArea === this.state.condition);
 
 
-//     changeItemConditions = (event) => {
-//         this.setState({
-//             Conditions: event.target.value
-//         })
-//         console.log(event);
-
-//     }
-//     changeItemLivingArea = (event) => {
-//         this.setState({
-//             search: event.target.value,
-//             LivingAreas: event.target.value
-//         })
-//         console.log(this.setState)
-
-//     }
-//     search = (event) => {
-//         this.setState({
-//             search: event.target.value
-            
-//         })
-       
-
-//     }
-
-//     render() {
-//         const { ads, search, activeUser, allUsers } = this.props;
-//         const { category, subCategory, Conditions, LivingAreas } = this.state;
-//         console.log(activeUser);
+        // const searchUpdated = ads.filter(ad => ad.Category && ad.SubCategory && ad.details === search)
+        // const searchUpdatedUi = searchUpdated.map(ad => <Col lg={3} md={4} sm={6}><AdCard ad={ad} /></Col>)
 
 
-//         // const searchUpdated = ads.filter(ad => ad.Category && ad.SubCategory && ad.details === search)
-//         // const searchUpdatedUi = searchUpdated.map(ad => <Col lg={3} md={4} sm={6}><AdCard ad={ad} /></Col>)
+        // const signupUser = !activeUser ? <Button href="#/signup" variant="secondary">signup</Button> : null
+        // const ??? = ???.map(ad => <Col lg={3} md={4} sm={6}><AdCard ad={ad} /></Col>)
 
+        // const filteredDropdown = ads.filter(ad => ad.CategoryId && ad.SubCategoryId === ad);
 
-//         // const signupUser = !activeUser ? <Button href="#/signup" variant="secondary">signup</Button> : null
-//         // const ??? = ???.map(ad => <Col lg={3} md={4} sm={6}><AdCard ad={ad} /></Col>)
+        return (
+            <div>
 
-//         // const filteredDropdown = ads.filter(ad => ad.CategoryId && ad.SubCategoryId === ad);
-//         // console.log(filteredDropdown)
+                <Dropdown>
+                    <Dropdown.Toggle className="m-3" variant="success" id="dropdown-basic">
+                        Category
+                    </Dropdown.Toggle >
+                    <Dropdown.Menu
+                    >
+                        {categoryOption}
+                    </Dropdown.Menu>
+                </Dropdown>
 
-//         return (
-//             <div>
+                <Dropdown>
+                    <Dropdown.Toggle className="m-3" variant="success" id="dropdown-basic">
+                        Sub-Category
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu
+                    >
+                        {subCategoryOption}
+                    </Dropdown.Menu>
+                </Dropdown>
 
-//                 <Dropdown>
-//                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-//                         Categorys
-//                     </Dropdown.Toggle>
-//                     <Dropdown.Menu
-//                         data={dataCategoriess}
-//                         textField="categoryName"
-//                         onChange={this.categoryChange}
-//                         value={category}
-//                     >
-//                         <Dropdown.Item defaultItem={ItemCategory} href="#/action-1"></Dropdown.Item>
-//                     </Dropdown.Menu>
-//                 </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle className="m-3" variant="success" id="dropdown-basic">
+                        Item Condition
+                     </Dropdown.Toggle>
+                    <Dropdown.Menu >
+                        {itemConditionOption}
+                    </Dropdown.Menu>
+                </Dropdown>
 
-//                 <Dropdown>
-//                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-//                         Sub-Categorys
-//                             </Dropdown.Toggle>
+                <Dropdown>
+                    <Dropdown.Toggle className="m-3" variant="success" id="dropdown-basic">
+                        Areas
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu >
+                        {dataLivingAreasOption}
+                    </Dropdown.Menu>
+                </Dropdown>
 
-//                     <Dropdown.Menu
-//                         data={dataSubCategorys}
-//                         textField="SubCategoryName"
-//                         onChange={this.subCategoryChange}
-//                         value={subCategory}
-//                     >
-//                         <Dropdown.Item defaultItem={ItemsubCategory} href="#/action-1"></Dropdown.Item>
-//                     </Dropdown.Menu>
-//                 </Dropdown>
+                <FormControl
+                    value={search} onChange={(e) => this.setState({ search: e.target.value })}
+                    type="text" placeholder="Search" className="mr-sm-2" />
+                <Button onClick={this.search} variant="outline-success">Search</Button>this.state.serch
+            </div>
+        );
+    }
+}
 
-//                 <Dropdown>
-//                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-//                         Item Condition
-//                      </Dropdown.Toggle>
-
-//                     <Dropdown.Menu
-//                         data={dataConditions}
-//                         onChange={this.changeItemConditions}
-//                         value={Conditions}
-//                     >
-//                         <Dropdown.Item textField="ConditionName"
-//                             defaultItem={ItemCondition}
-//                             href="#/action-1"></Dropdown.Item>
-//                     </Dropdown.Menu>
-//                 </Dropdown>
-
-//                 <Dropdown>
-//                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-//                         Areas
-//                      </Dropdown.Toggle>
-//                     <Dropdown.Menu
-//                         data={dataLivingAreas}
-//                         textField="LivingAreaName"
-//                         onChange={this.changeItemLivingArea}
-//                         value={LivingAreas}>
-//                         <Dropdown.Item defaultItem={ItemLivingArea} href="#/action-1"></Dropdown.Item>
-//                     </Dropdown.Menu>
-//                 </Dropdown>
-//             </div>
-//         );
-//     }
-// }
-
-// export default smartAgent;
+export default smartAgent;
