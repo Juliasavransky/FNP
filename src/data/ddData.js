c2dc9b6003d0eba1dc62a61edd48a468687bb16a
Julia savransky <savranskyj@gmail.com>
2020-08-27 11:40:41 +0300

    Changing the smart agan

--- src/data/ddData.js
+++ src/data/ddData.js
@@ -2,53 +2,53 @@
 // import jsonAds from '../src/data/Ads.json';
 
 const dataCategoriess = [
-    { categoryName: "Clothing", CategoryId: 1 },
-    { categoryName: "Toys and Games", CategoryId: 2 },
-    { categoryName: "For The Babys", CategoryId: 3 },
-    { categoryName: "For Moms", CategoryId: 4 },
+    { categoryName: "Clothing", categoryId: 1 },
+    { categoryName: "Toys and Games", categoryId: 2 },
+    { categoryName: "For The Babys", categoryId: 3 },
+    { categoryName: "For Moms", categoryId: 4 },
 
 ];
 
 const dataSubCategorys = [
-    { SubCategoryName: "Coats and Jackets", SubCategoryId: 11, CategoryId: 1 },
-    { SubCategoryName: "Special Events", SubCategoryId: 12, CategoryId: 1 },
-    { SubCategoryName: "Casual", SubCategoryId: 13, CategoryId: 1 },
-    { SubCategoryName: "Shoes", SubCategoryId: 14, CategoryId: 1 },
-    { SubCategoryName: "Clothing-Other", SubCategoryId: 15, CategoryId: 1 },
+    { subCategoryName: "Coats and Jackets", subCategoryId: 11, categoryId: 1 },
+    { subCategoryName: "Special Events", subCategoryId: 12, categoryId: 1 },
+    { subCategoryName: "Casual", subCategoryId: 13, categoryId: 1 },
+    { subCategoryName: "Shoes", subCategoryId: 14, categoryId: 1 },
+    { subCategoryName: "Clothing-Other", subCategoryId: 15, categoryId: 1 },
 
-    { SubCategoryName: "Dolls", SubCategoryId: 21, CategoryId: 2 },
-    { SubCategoryName: "Board Games", SubCategoryId: 22, CategoryId: 2 },
-    { SubCategoryName: "Books", SubCategoryId: 23, CategoryId: 2 },
-    { SubCategoryName: "Lego", SubCategoryId: 24, CategoryId: 2 },
-    { SubCategoryName: "ToysAndGames-Other", SubCategoryId: 25, CategoryId: 2 },
+    { subCategoryName: "Dolls", subCategoryId: 21, categoryId: 2 },
+    { subCategoryName: "Board Games", subCategoryId: 22, categoryId: 2 },
+    { subCategoryName: "Books", subCategoryId: 23, categoryId: 2 },
+    { subCategoryName: "Lego", subCategoryId: 24, categoryId: 2 },
+    { subCategoryName: "ToysAndGames-Other", subCategoryId: 25, categoryId: 2 },
 
-    { SubCategoryName: "Furniture", SubCategoryId: 31, CategoryId:3 },
-    { SubCategoryName: "Safety", SubCategoryId: 32, CategoryId:3 },
-    { SubCategoryName: "Carriage", SubCategoryId: 33, CategoryId:3 },
-    { SubCategoryName: "Playpen Cradle", SubCategoryId: 34, CategoryId:3 },
-    { SubCategoryName: "ForTheBaybys-Other", SubCategoryId: 35, CategoryId:3 },
+    { subCategoryName: "Furniture", subCategoryId: 31, categoryId:3 },
+    { subCategoryName: "Safety", subCategoryId: 32, categoryId:3 },
+    { subCategoryName: "Carriage", subCategoryId: 33, categoryId:3 },
+    { subCategoryName: "Playpen Cradle", subCategoryId: 34, categoryId:3 },
+    { subCategoryName: "ForTheBaybys-Other", subCategoryId: 35, categoryId:3 },
 
-    { SubCategoryName: "pregnancy clothes", SubCategoryId: 41, CategoryId:4 },
-    { SubCategoryName: "Breast Pumps", SubCategoryId: 42, CategoryId:4 },
-    { SubCategoryName: "books", SubCategoryId: 43, CategoryId:4 },
-    { SubCategoryName: "Supplements", SubCategoryId: 44, CategoryId:4 },
-    { SubCategoryName: "ForMoms-Other", SubCategoryId: 45, CategoryId:4 },
+    { subCategoryName: "pregnancy clothes", subCategoryId: 41, categoryId:4 },
+    { subCategoryName: "Breast Pumps", subCategoryId: 42, categoryId:4 },
+    { subCategoryName: "books", subCategoryId: 43, categoryId:4 },
+    { subCategoryName: "Supplements", subCategoryId: 44, categoryId:4 },
+    { subCategoryName: "ForMoms-Other", subCategoryId: 45, categoryId:4 },
 
 ];
 
 const dataConditions = [
-    { ConditionName: "New", ConditionId: 1 },
-    { ConditionName: "Good", ConditionId: 2 },
-    { ConditionName: "used-in a good condition", ConditionId: 3 }
+    { conditionName: "New", conditionId: 1 },
+    { conditionName: "Good", conditionId: 2 },
+    { conditionName: "used-in a good condition", conditionId: 3 }
    
 ];
 
 const dataLivingAreas = [
-    { LivingAreaName: "Center", LivingAreaId: 1 },
-    { LivingAreaName: "North", LivingAreaId: 2 },
-    { LivingAreaName: "Jerusalem", LivingAreaId: 3 },
-    { LivingAreaName: "South", LivingAreaId: 4 },
-    { LivingAreaName: "West", LivingAreaId: 5 },
+    { livingAreaName: "Center", livingAreaId: 1 },
+    { livingAreaName: "North", livingAreaId: 2 },
+    { livingAreaName: "Jerusalem", livingAreaId: 3 },
+    { livingAreaName: "South", livingAreaId: 4 },
+    { livingAreaName: "West", livingAreaId: 5 },
 
 ];
 
