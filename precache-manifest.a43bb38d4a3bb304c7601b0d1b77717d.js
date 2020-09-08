f8bd202aacb2fc82a847e36733db3ed50b985aab
Hanna Kantor <Hanna>
2020-09-08 11:43:03 +0300

    Updates

---
+++ precache-manifest.a43bb38d4a3bb304c7601b0d1b77717d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7866f128333a0dae4f2f8130b0081882",
+    "url": "/FNP/index.html"
+  },
+  {
+    "revision": "ff890bf12b32909ddecb",
+    "url": "/FNP/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "8e6e5aa1940720a58187",
+    "url": "/FNP/static/css/main.c1c12d25.chunk.css"
+  },
+  {
+    "revision": "ff890bf12b32909ddecb",
+    "url": "/FNP/static/js/2.6554e6ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FNP/static/js/2.6554e6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6e5aa1940720a58187",
+    "url": "/FNP/static/js/main.cbc2a5c2.chunk.js"
+  },
+  {
+    "revision": "717f5f8e8acb3540b37d",
+    "url": "/FNP/static/js/runtime-main.d357761b.js"
+  }
+]);
