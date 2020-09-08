d565e3c1e75e201371a19d1e0c61e0264ba73cee
Hanna Kantor <Hanna>
2020-09-08 22:38:02 +0300

    Adding SmartAgentCommand

---
+++ src/pages/SmartAgentCommand.js/SmartAgentCommand.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class SmartAgentCommand extends Component {
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Dropdown>
+            <br />
+            <h2>My Smart Agents</h2>
+            <br />
+            <Dropdown.Toggle variant="info" id="dropdown-basic">
+              Stuff I'm Looking For
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <CardColumns>
+            <Card className="shadow item well p-3 mb-5" key={sAgent.agentId}>
+              <Card.Body>
+                <Card.Title className="card text-center bg-white rounded">
+                  {" "}
+                  {sAgent.title}{" "}
+                </Card.Title>
+                <Card.Text className="card text-center">
+                  <Card.Text>Category: {categoryName}</Card.Text>
+                  <Card.Text>Sub-Category: {subCategoryName}</Card.Text>
+                  <Card.Text>Condition: {sAgent.conditionId}</Card.Text>
+                  <Card.Text>Living-Area: {sAgent.livingAreaId}</Card.Text>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardColumns>
+        </Container>
+        ads={ads}
+          handleLogin={this.props.handleLogin}
+          allUsers={allUsers}
+          handleLogout={this.props.handleLogout}
+          handleCreatNewAd={this.props.handleCreatNewAd}
+          handleCreatSmartNewAgent={this.props.handleCreatSmartNewAgent}
+          activeUser={activeUser}
+      </div>
+    );
+  }
+}
+
+export default SmartAgentCommand;
